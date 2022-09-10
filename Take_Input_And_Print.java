8026c05737d8b22d181f02db830126e0504af956
harikiran19 <96737891+harikiran19@users.noreply.github.com>
2022-09-10 11:45:19 +0530

    import java.util.Scanner; class Tiap {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         String st;         st=sc.nextLine();         System.out.println(st);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Tiap
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        String st;
+        st=sc.nextLine();
+        System.out.println(st);
+    }
+}
