9a06893d23d54a3d30999eb8c2bc96f23d4f87eb
harikiran19 <96737891+harikiran19@users.noreply.github.com>
2022-09-30 15:53:08 +0530

    import java.util.Scanner; class pattern {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++)         {             for(int j=0;j<n;j++)             {                 System.out.print((i+1)+" ");             }             System.out.print(" ");         }     } }

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class pattern
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print((i+1)+" ");
+            }
+            System.out.print("
+");
+        }
+    }
+}
