3ccca3ac4a08dc850ed613770f19ee82d4fee796
harikiran19 <96737891+harikiran19@users.noreply.github.com>
2022-10-16 10:34:32 +0530

    import java.util.*; class Min_scale {     public static int gcd(int a,int b)     {         while(a!=b){             if(a>b)                 a=a-b;             else                 b=b-a;         }         return a;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int []arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         int h=gcd(arr[0],arr[1]);         for(int i=2;i<n;i++){             h=gcd(h,arr[i]);         }         System.out.print(h);     } }

---
+++ Minimum_Scale_length.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Min_scale
+{
+    public static int gcd(int a,int b)
+    {
+        while(a!=b){
+            if(a>b)
+                a=a-b;
+            else
+                b=b-a;
+        }
+        return a;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int h=gcd(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            h=gcd(h,arr[i]);
+        }
+        System.out.print(h);
+    }
+}
