023a16919105f0b95c340448423b1c8f77dba522
harikiran19 <96737891+harikiran19@users.noreply.github.com>
2022-10-27 11:30:55 +0530

    import java.util.*; class arokgnas {     public static void main(String asd[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         for(int i=0;i<a;i++)         {             int b = scan.nextInt();             int c = scan.nextInt();             System.out.println(b+c);         }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class arokgnas
+{
+    public static void main(String asd[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        for(int i=0;i<a;i++)
+        {
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+            System.out.println(b+c);
+        }
+    }
+}
