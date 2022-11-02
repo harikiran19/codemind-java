7ef186ea9cb5c23d29417b45202d4285315e18a8
harikiran19 <96737891+harikiran19@users.noreply.github.com>
2022-11-02 16:44:13 +0530

    import java.util.Scanner; class Even_btw_even {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)             arr[i]=sc.nextInt();         int c=0;         for(int i=1;i<n-1;i++){             if(arr[i]%2==0 && arr[i-1]%2==0 && arr[i+1]%2==0)                 c++;         }         System.out.print(c);     } }

---
+++ count_even_numbers_between_even_numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Even_btw_even
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int c=0;
+        for(int i=1;i<n-1;i++){
+            if(arr[i]%2==0 && arr[i-1]%2==0 && arr[i+1]%2==0)
+                c++;
+        }
+        System.out.print(c);
+    }
+}
