283c32dff868279346ccedce9067f09b5b9e2256
harikiran19 <96737891+harikiran19@users.noreply.github.com>
2022-11-02 16:55:29 +0530

    import java.util.Scanner; class Count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)             arr[i]=sc.nextInt();         int s=sc.nextInt();         int c=0;         for(int i=0;i<n;i++)             if(arr[i]==s)                 c++;         System.out.print(c);     }
    }

---
+++ count_of_given_element_in_an_array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int s=sc.nextInt();
+        int c=0;
+        for(int i=0;i<n;i++)
+            if(arr[i]==s)
+                c++;
+        System.out.print(c);
+    }
+    
+}
