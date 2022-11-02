e220109b71f019e703d67ede6650819b821d52e4
harikiran19 <96737891+harikiran19@users.noreply.github.com>
2022-11-02 16:42:39 +0530

    import java.util.Scanner; class Last_odd {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)             arr[i]=sc.nextInt();         int ind=-1;         for(int i=0;i<n;i++)             if(arr[i]%2!=0)                 ind=i;         System.out.print(ind);     } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Last_odd
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int ind=-1;
+        for(int i=0;i<n;i++)
+            if(arr[i]%2!=0)
+                ind=i;
+        System.out.print(ind);
+    }
+}
